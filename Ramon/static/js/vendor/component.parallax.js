ff5e3b9a184041671958e5a8d4ad97ad83d02ad4
Ramon <ramon.weijenbarg@outlook.com>
2015-03-30 11:37:33 +0200

    BugFix

--- Ramon/static/js/vendor/component.parallax.js
+++ Ramon/static/js/vendor/component.parallax.js
@@ -1,4 +1,4 @@
 $(function mapParallax(x, in_min,  in_max,  out_min,  out_max){
     var Waarde = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    return Waarde 
+    return Waarde;
 })
