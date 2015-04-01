45d6c851808c89e41f35bb3eff9c6c6701f66af4
Teun van Olphen <teunvanolphen@web-geek.nl>
2015-04-01 16:58:44 +0200

    oefening 2 afgerond

--- Teun/Oefeningen/oefening2/js/app.js
+++ Teun/Oefeningen/oefening2/js/app.js
@@ -3,16 +3,32 @@ $(function() {
 
 	var vegOn = $("button#vegOn")
 	vegOn.click(function() {
-		/* Act on the event */
-		console.log("button vegOn clicked")
+		// console.log("button vegOn clicked")
+		
+		//detach all fish meals
 		$f = $(".fish").parent().parent().detach();
-		$hamB = $(".hamburger").replaceWith("<li class='portobello'><em>Portobello Mushroom</em></li>");
+		//replace hamburger with Portobello Mushroom
+		$(".hamburger").replaceWith("<li class='portobello'><em>Portobello Mushroom</em></li>");
+		//replace meat with tofu
+		$(".meat").after('<li class="tofu">Tofu</li>')
+		$meat = $(".meat").detach();
+		$(".tofu").parent().parent().addClass('veg_leaf');
 	});
 
 	var restoreBtn = $("button#restoreMe")
 	restoreBtn.click(function() {
-		/* Act on the event */
-		console.log("restore btn clicked");
+		// console.log("restore btn clicked");
+		
+		//put .fish back
+		$(".menu_entrees li").first().before($f);
+		//replace Portobello with hamburger
+		$(".portobello").replaceWith("<li class='hamburger'>hamburger</li>");
+		//loop true tofu and replace tofu[i] with meat[i]
+		$(".tofu").each(function(i) {
+			$(this).after( $meat[i] );
+		});
+		$(".tofu").remove();
+		$(".veg_leaf").removeClass();
 	});
 
 	
