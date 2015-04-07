6d6c16e2d5ef70004f61263c782a73dc8396183b
Teun van Olphen <teunvanolphen@web-geek.nl>
2015-04-07 13:51:49 +0200

    music btn

--- Samen/static/js/component.music.js
+++ Samen/static/js/component.music.js
@@ -1,20 +1,7 @@
 $(function () {
- var audioElement = document.createElement('audio');
-  audioElement.setAttribute('src', 'audio.mp3');
-  audioElement.setAttribute('autoplay', 'autoplay');
-  //audioElement.load()
 
-  $.get();
+  $('#music_btn').click(function(){
+        $(this).toggleClass("mute");
+    });
 
-  audioElement.addEventListener("load", function() {
-      audioElement.play();
-  }, true);
-
-  $('.music_btn').toggle(function() {
-      // audioElement.play();
-      console.log("click1");
-  }, function() {
-	    // audioElement.pause();
-	  	console.log("click2");
-  });
 });
