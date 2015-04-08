673aef4d20e0f444283f763a49d043473c023b21
Teun van Olphen <teunvanolphen@web-geek.nl>
2015-04-08 13:46:53 +0200

    music update

--- Teun/static/js/component.music.js
+++ Teun/static/js/component.music.js
@@ -1,26 +1,27 @@
 $(function () {
  var audioElement = document.createElement('audio');
-  audioElement.setAttribute('src', 'audio.mp3');
+  audioElement.setAttribute('src', 'static/audio/bensound-funnysong.mp3');
   audioElement.setAttribute('autoplay', 'autoplay');
   audioElement.load()
 
   $.get();
 
-  audioElement.addEventListener("load", function() {
-      audioElement.play();
-  }, true);
+   audioElement.addEventListener("load", function() {
+            audioElement.play();
+        }, true);
 
- //  $('.music_btn').toggle(function() {
- //      // audioElement.play();
- //      console.log("click1");
- //  }, function() {
-	//     // audioElement.pause();
-	//   	console.log("click2");
- //  });
-
-  $('#music_btn').click(function(){
-        $(this).toggleClass("mute");
-    });
+        var musicState = true;
+        $('#music_btn').click(function() {
+            if (musicState == true) {
+              audioElement.pause();
+              musicState = false;
+            }
+            else
+            {
+              audioElement.play();
+              musicState = true;
+            }
+        });
 
 
 });
