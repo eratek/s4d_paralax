db86986d254233c82ffe0e9c5fe4349c37031315
Teun van Olphen <teunvanolphen@web-geek.nl>
2015-04-09 12:48:47 +0200

    update from samen

--- Teun/static/js/component.fastscroll.js
+++ Teun/static/js/component.fastscroll.js
@@ -1,5 +1,8 @@
-// $("#btnScroll").click(function(event){
-//       event.preventDefault();
-//       $("body").animate({"scrollTop": "3280px"},6000)
-//       $("#btnScroll").animate({ opacity: 0 });
-//  });
+$("#btnScroll").click(function(event){
+      event.preventDefault();
+      $("body").animate({"scrollTop": "3280px"},6000)
+ });
+
+$("#btnScroll").click(function(){
+    $("#btnScroll").animate({ opacity: 0 });
+});
