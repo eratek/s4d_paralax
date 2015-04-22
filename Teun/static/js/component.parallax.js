1decb6f7f1f79d42fc45f9b89f4fbc13938e992b
Teun van Olphen <prive@teunvanolphen.nl>
2015-04-22 16:52:32 +0200

    last version

--- Teun/static/js/component.parallax.js
+++ Teun/static/js/component.parallax.js
@@ -1,150 +1,48 @@
-var easingFunctions = {
-    // x: optioneel t: current time, b: begInnIng value, c: change In value, d: duration
-    easeInQuad: function (x, t, b, c, d) {
-        return c*(t/=d)*t + b;
-    },
-    easeOutQuad: function (x, t, b, c, d) {
-        return -c *(t/=d)*(t-2) + b;
-    },
-    easeInOutQuad: function (x, t, b, c, d) {
-        if ((t/=d/2) < 1) return c/2*t*t + b;
-        return -c/2 * ((--t)*(t-2) - 1) + b;
-    },
-    easeInCubic: function (x, t, b, c, d) {
-        return c*(t/=d)*t*t + b;
-    },
-    easeOutCubic: function (x, t, b, c, d) {
-        return c*((t=t/d-1)*t*t + 1) + b;
-    },
-    easeInOutCubic: function (x, t, b, c, d) {
-        if ((t/=d/2) < 1) return c/2*t*t*t + b;
-        return c/2*((t-=2)*t*t + 2) + b;
-    },
-    easeInQuart: function (x, t, b, c, d) {
-        return c*(t/=d)*t*t*t + b;
-    },
-    easeOutQuart: function (x, t, b, c, d) {
-        return -c * ((t=t/d-1)*t*t*t - 1) + b;
-    },
-    easeInOutQuart: function (x, t, b, c, d) {
-        if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
-        return -c/2 * ((t-=2)*t*t*t - 2) + b;
-    },
-    easeInQuint: function (x, t, b, c, d) {
-        return c*(t/=d)*t*t*t*t + b;
-    },
-    easeOutQuint: function (x, t, b, c, d) {
-        return c*((t=t/d-1)*t*t*t*t + 1) + b;
-    },
-    easeInOutQuint: function (x, t, b, c, d) {
-        if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
-        return c/2*((t-=2)*t*t*t*t + 2) + b;
-    },
-    easeInSine: function (x, t, b, c, d) {
-        return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
-    },
-    easeOutSine: function (x, t, b, c, d) {
-        return c * Math.sin(t/d * (Math.PI/2)) + b;
-    },
-    easeInOutSine: function (x, t, b, c, d) {
-        return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
-    },
-    easeInExpo: function (x, t, b, c, d) {
-        return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
-    },
-    easeOutExpo: function (x, t, b, c, d) {
-        return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
-    },
-    easeInOutExpo: function (x, t, b, c, d) {
-        if (t==0) return b;
-        if (t==d) return b+c;
-        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
-    },
-    easeInCirc: function (x, t, b, c, d) {
-        return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
-    },
-    easeOutCirc: function (x, t, b, c, d) {
-        return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
-    },
-    easeInOutCirc: function (x, t, b, c, d) {
-        if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
-        return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
-    },
-    easeInElastic: function (x, t, b, c, d) {
-        var s=1.70158;var p=0;var a=c;
-        if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-        if (a < Math.abs(c)) { a=c; var s=p/4; }
-        else var s = p/(2*Math.PI) * Math.asin (c/a);
-        return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
-    },
-    easeOutElastic: function (x, t, b, c, d) {
-        var s=1.70158;var p=0;var a=c;
-        if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-        if (a < Math.abs(c)) { a=c; var s=p/4; }
-        else var s = p/(2*Math.PI) * Math.asin (c/a);
-        return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
-    },
-    easeInOutElastic: function (x, t, b, c, d) {
-        var s=1.70158;var p=0;var a=c;
-        if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
-        if (a < Math.abs(c)) { a=c; var s=p/4; }
-        else var s = p/(2*Math.PI) * Math.asin (c/a);
-        if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
-        return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
-    },
-    easeInBack: function (x, t, b, c, d, s) {
-        if (s == undefined) s = 1.70158;
-        return c*(t/=d)*t*((s+1)*t - s) + b;
-    },
-    easeOutBack: function (x, t, b, c, d, s) {
-        if (s == undefined) s = 1.70158;
-        return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
-    },
-    easeInOutBack: function (x, t, b, c, d, s) {
-        if (s == undefined) s = 1.70158; 
-        if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
-        return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
-    },
-    easeInBounce: function (x, t, b, c, d) {
-        return c - easingFunctions.easeOutBounce (x, d-t, 0, c, d) + b;
-    },
-    easeOutBounce: function (x, t, b, c, d) {
-        if ((t/=d) < (1/2.75)) {
-            return c*(7.5625*t*t) + b;
-        } else if (t < (2/2.75)) {
-            return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
-        } else if (t < (2.5/2.75)) {
-            return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
-        } else {
-            return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
-        }
-    },
-    easeInOutBounce: function (x, t, b, c, d) {
-        if (t < d/2) return easingFunctions.easeInBounce (x, t*2, 0, c, d) * .5 + b;
-        return easingFunctions.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
-    }
+function getRandom(num) {
+    var randomNum = Math.floor(Math.random() * num);
+    return randomNum;
+}
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 
-function mapPar(x, in_min,  in_max,  out_min,  out_max, easeFunction){
+function clouds(){
+    for (var i = randomIntFromInterval(20, 35) ; i >= 0; i--) {
+        var randomN = getRandom(3);
+        if (randomN == 0) {
+            cloud("small",2100,1040,100,120,190);
+        }
+        else if (randomN == 1) {
+          cloud("medium",2100,930,100,200,300);
+        }
+        else {
+          cloud("large",1900,730,100,300,500);
+        }
+    };
+}
+
+function cloud(size,t,l,z,min,max){
+    $("#clouds").append("<img src='static/img/cloud.png' class='cloud_"+size+"'/>");   
+    $( ".cloud_"+size ).each(function( index ) {
+      $(this).css({
+            top: getRandom(t),
+            left: getRandom(l),
+            "z-index": getRandom(z),
+            width: randomIntFromInterval(min, max)
+        });
+    });
+}
+
+function mapPar(x, in_min,  in_max,  out_min,  out_max){
     if( x < in_min) {
         return out_min;
     }
     if( x > in_max) {
         return out_max;
     }
-
-    var easingInput = (x-in_min) / (in_max - in_min);
-
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    // var outputVoorEasing = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    // if(easeFunction !== undefined) {
-    //     // t: current time, b: begInnIng value, c: change In value, d: duration
-    //     return  easeFunction(0,easingInput,0,1,1) * outputVoorEasing;
-    // } else {
-    //     return outputVoorEasing;
-    // }
 }
 
 $( function() {
@@ -160,47 +58,49 @@ $( function() {
     // });
 
     $(window).scroll( function( eventInfo ) {
-
         var scrollPos =  $("body").scrollTop();
-        console.log(scrollPos);
+        
+        var lastScroll = 0
+        if (scrollPos > lastScroll) {
+            lastScroll = lastScroll + scrollPos;
+        }
+        else {
+            lastScroll = lastScroll
+        }
+
+        console.log("lastScroll: "+lastScroll);
+        console.log("scrollPos: "+scrollPos)
+
+        function movePar(startValue, endValue, startResult, endResult) {
+            if (scrollPos >= startValue) {
+                return mapPar(scrollPos, startValue,endValue, startResult,endResult);
+            }
+        }
+        // console.log(scrollPos);
 
         // charByte
-        var fallChar    = mapPar(scrollPos, 0,2400, 200,2800);
-        var moveRightChar = mapPar(scrollPos, 2400,2800, 0,400);
-        // var scaleCharHeight = mapPar(scrollPos, 2400,2800, 100,100);
+        var fallChar = movePar(0,2400,200,2800);
+        var moveRightChar = movePar(2400,2800,0,400);
+        $("#charByte").css({ top: fallChar });
         
         // StartScene
-        var moveStartSceneOut = mapPar(scrollPos, 3000,4000, 0,928);
+        var moveStartSceneOut = movePar(3000,4000,0,928);
+        $("#backgroundStartScene").css({ right: moveStartSceneOut });
 
         // scene1
-        var moveSceneOne = mapPar(scrollPos, 3000,4000, -928,0);
+        var moveSceneOne = movePar(3000,4000, -928,0);
+        $("#backgroundScene1").css({ right: moveSceneOne });
         // var fadeInQuestionOneBg = mapPar(scrollPos, 4300,4600, 0,1);
         // var fadeInQuestionOne = mapPar(scrollPos, 4600,4700, 0,1);
         // var fadeInChooseOptions = mapPar(scrollPos, 4400,4600, 0,1);
-        var moveSceneOneOut = mapPar(scrollPos, 5000,6000, 0,928);
+        var moveSceneOneOut = movePar(5000,6000,0,928)
+        $("#backgroundScene1").css({ right: moveSceneOneOut });
 
         // scene2
-        var moveSceneTwo = mapPar(scrollPos, 5000,6000, -928,0);
+        var moveSceneTwo = movePar(5000,6000,-928,0);
+        $("#backgroundScene2_1").css({ right: moveSceneTwo }); 
         // var fadeSceneOne = mapPar(scrollPos, 2725,2785, 0,1)
-        
-        
-        $("#charByte").css({top: fallChar});
 
-        $("#backgroundStartScene").css({
-            right: moveStartSceneOut
-        });
-        
-        $("#backgroundScene1").css({
-            right: moveSceneOne
-        });
-
-        $("#backgroundScene1").css({
-            right: moveSceneOneOut
-        });
-
-        $("#backgroundScene2_1").css({
-            right: moveSceneTwo
-          }); 
 
         // $("#backgroundScene2_2").css({
         //     left: moveSceneTwo
@@ -256,11 +156,11 @@ $( function() {
             });
         }
 
-        if (scrollPos >= 4300) {
-            $("body").css({
-                height: 7678 //+678 = scrollPos 7000
-            })
-        };
+        // if (scrollPos >= 4300) {
+        //     $("body").css({
+        //         height: 8678 //+678 = scrollPos 7000
+        //     })
+        // };
 
         
 
@@ -297,41 +197,3 @@ $( function() {
         // }
     })
 })
-
-function getRandom(num) {
-    var randomNum = Math.floor(Math.random() * num);
-    return randomNum;
-}
-
-function randomIntFromInterval(min,max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-
-function clouds(){
-    for (var i = randomIntFromInterval(20, 35) ; i >= 0; i--) {
-        var randomN = getRandom(3);
-        if (randomN == 0) {
-            cloud("small",2100,1040,100,120,190);
-        }
-        else if (randomN == 1) {
-          cloud("medium",2100,930,100,200,300);
-        }
-        else {
-          cloud("large",1900,730,100,300,500);
-        }
-    };
-}
-
-function cloud(size,t,l,z,min,max){
-    $("#clouds").append("<img src='static/img/cloud.png' class='cloud_"+size+"'/>");   
-    $( ".cloud_"+size ).each(function( index ) {
-      $(this).css({
-            top: getRandom(t),
-            left: getRandom(l),
-            "z-index": getRandom(z),
-            width: randomIntFromInterval(min, max)
-        });
-    });
-}
-
